9b39b1ae3dae9f45e98c43f260da5d7212e9327d
VictorCcorimanya <raulccorimanyaalfaro@gmail.com>
2021-12-02 00:17:31 -0500

    [FIX] update conn

--- src/app.php
+++ src/app.php
@@ -5,7 +5,11 @@ $dbName = getenv('CLOUDSQL_DATABASE_NAME');
 $dbUser = getenv('CLOUDSQL_USER');
 $dbPass = getenv('CLOUDSQL_PASSWORD');
 
+$connection = mysqli_connect($dbConn, $dbUser, $dbPass,$dbName);
 
-$pdo = new PDO($dbConn, $dbUser, $dbPass);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+if(!$connection){
+  echo "Error!" .mysqli_connect_error();
+}else {
+  echo "Succces";
+}
 
