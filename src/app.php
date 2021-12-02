6cfdf91951ce52417650a52ccbd2c5a46b70b9b2
VictorCcorimanya <raulccorimanyaalfaro@gmail.com>
2021-12-02 00:51:21 -0500

    [FIX] update unix socket3

--- src/app.php
+++ src/app.php
@@ -1,11 +1,12 @@
 <?php
 
+$dbHName = getenv('CLOUDSQL_HOST_NAME');
 $dbConn = getenv('CLOUDSQL_CONNECTION_NAME');
 $dbName = getenv('CLOUDSQL_DATABASE_NAME');
 $dbUser = getenv('CLOUDSQL_USER');
 $dbPass = getenv('CLOUDSQL_PASSWORD');
 
-$connection = mysqli_connect(null, $dbUser, $dbPass,$dbName,null,$dbConn);
+$connection = mysqli_connect($dbHName, $dbUser, $dbPass,$dbName,null,$dbConn);
 
 if(!$connection){
   echo "Error!" . mysqli_connect_error();
